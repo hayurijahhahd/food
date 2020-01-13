9bfa1da4c55a0fafcbcc291a6cb9356c53212eed
Einnlleinhatt <hayurijahhahd@gmail.com>
2020-01-14 02:28:37 +0900

    Nothing

---
+++ App.js
@@ -0,0 +1,14 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Search from './src/screens/Search';
+
+const navigator = createStackNavigator({
+	Search: Search
+}, {
+	initialRouteName: 'Search',
+	defaultNavigationOptions: {
+		title: 'Business Search'
+	}
+});
+
+export default createAppContainer(navigator);
